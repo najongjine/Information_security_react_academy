88673d864f5bd9c40c329fe08ab094eca428188f
najongjine <najongjin3@hotmail.com>
2025-06-23 14:28:53 +0900

    .

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,39 +2,59 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Home: React.FC = () => {
-  const[lottoNumbers,setLottoNumbers]=useState<number[]>([]);
-  const generateLottoNumbers=()=>{
-    let numbers:number[]=[];
-    for(let i=1;i<=45;i++){
+  const [lottoNumbers, setLottoNumbers] = useState<number[]>([]);
+  // 최소값(min)과 최대값(max) 사이의 랜덤 정수 만들기
+  function getRandomInt(min: number, max: number): number {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  function shuffleNumList(numbers: number[], shuffleRepeats: number) {
+    for (let a = 0; a < shuffleRepeats; a++) {
+      for (let i = numbers.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [numbers[i], numbers[j]] = [numbers[j], numbers[i]]
+      }
+    }
+    return numbers
+  }
+  const generateLottoNumbers = () => {
+    let numbers: number[] = [];
+    for (let i = 1; i <= 45; i++) {
       numbers.push(i)
     }
-    for(let i=numbers.length-1;i>0;i--){
-      const j=Math.floor(Math.random()*(i+1));
-      [numbers[i],numbers[j]]=[numbers[j],numbers[i]]
-    }
+    numbers = shuffleNumList(numbers, getRandomInt(5, 99))
+    numbers = numbers.slice(0, 6)
     setLottoNumbers(numbers)
   }
 
   return (
-    <>
-      <h2 className="">
-        홈 화면이에요!
-      </h2>
-      <p>로또번호 🎉</p>
-      <div>
-        {lottoNumbers.map((num,index)=>(
-          <span>{num}</span>
-        ))}
-      </div>
-      <Link to="/todo_list">
+    <div className="min-h-screen bg-gradient-to-r from-blue-500 to-purple-600 flex items-center justify-center">
+      <div className="text-white text-center p-8 rounded-lg shadow-xl bg-opacity-70">
+        <h1 className="text-4xl font-bold mb-6">로또 번호 추천</h1>
         <button
-          className=""
           onClick={generateLottoNumbers}
+          className="px-6 py-2 bg-yellow-500 text-black rounded-lg text-lg hover:bg-yellow-600 transition-all"
         >
-          번호생성
+          로또 번호 생성
         </button>
-      </Link>
-    </>
+        <div className="mt-8">
+          {lottoNumbers.length > 0 && (
+            <div className="text-2xl font-semibold">
+              <p>추천 번호:</p>
+              <div className="mt-2 text-3xl font-bold flex justify-center gap-4">
+                {lottoNumbers.map((number, index) => (
+                  <span
+                    key={index}
+                    className="w-12 h-12 flex items-center justify-center bg-white text-black rounded-full"
+                  >
+                    {number}
+                  </span>
+                ))}
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
   );
 };
 
