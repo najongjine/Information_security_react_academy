ec6052298d1a4df00856ad33cc85f19fa20ba070
najongjine <najongjin3@hotmail.com>
2025-06-23 13:44:40 +0900

    .

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Home: React.FC = () => {
+  const[lottoNumbers,setLottoNumbers]=useState<number[]>([]);
+  const generateLottoNumbers=()=>{
+    let numbers:number[]=[];
+    for(let i=1;i<=45;i++){
+      numbers.push(i)
+    }
+    for(let i=numbers.length-1;i>0;i--){
+      const j=Math.floor(Math.random()*(i+1));
+      [numbers[i],numbers[j]]=[numbers[j],numbers[i]]
+    }
+    setLottoNumbers(numbers)
+  }
+
   return (
     <>
       <h2 className="min-h-screen bg-gray-100 flex items-center justify-center">
